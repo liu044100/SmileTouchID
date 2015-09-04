8148e1a0a4177200c59ed47d25b05f9743a93f2d
rain <liu044100@gmail.com>
2015-09-04 10:23:46 +0900

    security refine
    add cover imageView when app enter background

--- Example/TouchID/AppDelegate.m
+++ Example/TouchID/AppDelegate.m
@@ -13,7 +13,9 @@
 
 @end
 
-@implementation AppDelegate
+@implementation AppDelegate{
+    UIImageView *_coverImageView;
+}
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
@@ -32,14 +34,34 @@
     return YES;
 }
 
+-(void)addCoverImageView{
+    [self removeCoverImageView];
+    _coverImageView = [[UIImageView alloc]initWithFrame:[self.window frame]];
+    _coverImageView.contentMode = UIViewContentModeScaleAspectFill;
+    UIImage *image = [UIImage imageNamed:@"backgroundImage"];
+    [_coverImageView setImage:image];
+    [self.window addSubview:_coverImageView];
+}
+
+-(void)removeCoverImageView{
+    if(_coverImageView != nil) {
+        [_coverImageView removeFromSuperview];
+        _coverImageView = nil;
+    }
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    if ([SmileAuthenticator hasPassword]) {
+        [self addCoverImageView];
+    }
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
@@ -48,6 +70,9 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    if ([SmileAuthenticator hasPassword]) {
+        [self removeCoverImageView];
+    }
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
